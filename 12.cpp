a5dec63ffae4cd5b22922904a6f7997028d1ae97
Grumzy <90696311+Grumzy@users.noreply.github.com>
2023-06-23 02:11:31 +0800

    Update 12.cpp

--- 12.cpp
+++ 12.cpp
@@ -18,8 +18,8 @@ void Queen(int k)
 			if (k == n)
 			{
 				counter++;
-				//for (j = 1; j <= n; j++) printf("%2d ", P[j]);
-				//printf("\n");
+				for (j = 1; j <= n; j++) printf("%2d ", P[j]);
+				printf("\n");
 			}
 			else Queen(k + 1);
 			H[i] = 0; R[i - k + 21] = 0; L[i + k] = 0;
